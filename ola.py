f4a38dcf935219a78b2fa9999103967340286945
Gabrielle-Cartaxo <gabrielle.cartaxo@sou.inteli.edu.br>
2024-02-06 15:26:48 -0300

    funcao extra adicionada

--- ola.py
+++ ola.py
@@ -1,4 +1,5 @@
 from funcoes import soma #pega a função do arquivo
 print("Olá, mundo!")
 resultado = soma(5, 9.3)
-print(f"O resultado da soma é {resultado}")
+subresultado = soma(3, 7)
+print(f"O resultado da soma é {resultado}, e da subtração é {subresultado}!")
