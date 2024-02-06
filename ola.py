2dc8bf642bf35be1aa7031c252a0a97a7bf4c84d
Gabrielle-Cartaxo <gabrielle.cartaxo@sou.inteli.edu.br>
2024-02-06 15:31:20 -0300

    test

--- ola.py
+++ ola.py
@@ -1,5 +1,5 @@
 from funcoes import soma #pega a função do arquivo
 print("Olá, mundo!")
-resultado = soma(5, 9.3)
+resultado = soma(5, 9)
 subresultado = soma(3, 7)
 print(f"O resultado da soma é {resultado}, e da subtração é {subresultado}!")
